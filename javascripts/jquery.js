d2133e043d108cec1e60ca84033661492d96f8dc
Esop Baek <esop.baek@gmail.com>
2014-08-12 04:56:51 -0400

    Improve styling and javascript animation

--- javascripts/jquery.js
+++ javascripts/jquery.js
@@ -2,19 +2,42 @@ $(document).ready(function(){
 
   $("button").on("click", function(event){
     $("body").toggleClass("open-menu");
+    $("button").hide();
   });
 
   $(".push").on("click", function(event){
 
     if($(event.target).is(".push")){
       $("body").removeClass("open-menu");
+      $("button").show("slow");
     }
 
   });
 
   $("li.home").on("click", function(event){
+    event.preventDefault;
+    $("body").toggleClass("open-menu");
+    $("div.home-page").show("fast");
+    $("div.projects").hide("slow");
+    $("div.resume").hide("slow")
+    $("button").show("slow");
+  });
+
+  $("li.projects").on("click", function(event){
   	event.preventDefault;
   	$("body").toggleClass("open-menu");
-  	$(".home-page").addClass("hidden");
+  	$("div.home-page").hide("slow");
+    $("div.projects").show("fast");
+    $("div.resume").hide("slow")
+    $("button").show("fast");
+  });
+
+  $("li.resume").on("click", function(event){
+    event.preventDefault;
+    $("body").toggleClass("open-menu");
+    $("div.home-page").hide("slow");
+    $("div.projects").hide("slow");
+    $("div.resume").show("fast")
+    $("button").show("fast");
   });
 });
